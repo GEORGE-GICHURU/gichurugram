8f4a3dcf4f558a5debd5e9e124b5da4abdf39801
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-06-06 15:40:34 +0300

    message model to be displayed in the admin after making migrations

--- directmessages/models.py
+++ directmessages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
-# from notifications.signals import notif
+# from notifications.signals import notify
 
 
 class Message(models.Model):
