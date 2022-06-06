a00dbaa8cccbfc5e9c4b9cebba3b0a5fbe8e76da
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-06-06 16:04:47 +0300

    made tests for users

--- users/tests.py
+++ users/tests.py
@@ -13,4 +13,3 @@ class MessageTestCase(TestCase):
         message = self.create_message()
         self.assertTrue(isinstance(message, Message))
         self.assertEqual(message.__str__(), message.content)
-#
