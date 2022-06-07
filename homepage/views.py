b25f179bf366bd0b48e553faa7145229e5a72d45
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2022-06-07 14:58:16 +0300

    Initial commit

--- homepage/views.py
+++ homepage/views.py
@@ -20,7 +20,6 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'homepage/home.html'
     ordering = ['-date_posted']
     context_object_name = 'posts'
-    paginate_by = 6
 
 
     def get_context_data(self, **kwargs):
